01e6a02ac894ce9deb30f8bcabfb0745c8c7fb78
Aurio Rajaa <riorajaa2018@gmail.com>
2023-12-01 20:04:58 +0700

    add hiversine formula

--- src/com/main/City.java
+++ src/com/main/City.java
@@ -1,15 +1,15 @@
 package com.main;
 
 public class City {
-	
+
 	private static final double EARTH_EQUATORIAL_RADIUS = 6378.1370D;
-	private static final double CONVERT_DEGREES_TO_RADIANS = Math.PI/180D;
+	private static final double CONVERT_DEGREES_TO_RADIANS = Math.PI / 180D;
 	private static final double CONVERT_KM_TO_MILES = 0.621371;
-	
+
 	private double longitude;
 	private double latitude;
 	private String name;
-	
+
 	public City(double longitude, double latitude, String name) {
 		super();
 		this.longitude = longitude * CONVERT_DEGREES_TO_RADIANS;
@@ -28,6 +28,19 @@ public class City {
 	public String getName() {
 		return name;
 	}
-	
-	
+
+	public String toString() {
+		return getName();
+	}
+
+	// Method that represent Haversine Formula
+	public double measureDistance(City city) {
+		double deltaLongitude = city.getLongitude() - this.getLongitude();
+		double deltaLatitude = city.getLatitude() - this.getLatitude();
+
+		double a = Math.pow(Math.sin(deltaLatitude / 2D), 2D) + Math.cos(this.getLatitude())
+				* Math.cos(city.getLatitude()) * Math.pow(Math.sin(deltaLongitude / 2D), 2D);
+
+		return CONVERT_KM_TO_MILES * EARTH_EQUATORIAL_RADIUS * 2D * Math.atan2(Math.sqrt(a), Math.sqrt(1D - a));
+	}
 }
--- src/com/main/GeneticAlgorithm.java
+++ src/com/main/GeneticAlgorithm.java
@@ -1,5 +1,5 @@
 package com.main;
 
 public class GeneticAlgorithm {
-
+    public static final int POPULATION_SIZE = 8;
 }
